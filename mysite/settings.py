fdb7eef489c138aa1a197bd2a2dd847770976f75
D Herndon <djazmine@msn.com>
2017-01-30 15:40:23 -0500

    Debug switch f

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2z3#z)j%61k5xiv-=p_udx@neg%eb12!6!m%l9v$^s1bmta=&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
